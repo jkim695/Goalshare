cfdb79db87d6bcf0e0bf380748ebd653289b5879
jkim236 <jkim236@uw.edu>
2022-11-19 12:41:58 -0800

    Initial Commit

---
+++ Goalshare tree2/Goalshare_tree2App.swift
@@ -0,0 +1,17 @@
+//
+//  Goalshare_tree2App.swift
+//  Goalshare tree2
+//
+//  Created by Joshua Kim on 11/19/22.
+//
+
+import SwiftUI
+
+@main
+struct Goalshare_tree2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
