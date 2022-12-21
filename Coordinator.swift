f452764fc0df3600fc9bb9b23592879c62a9acb1
jcthewizard <joshchou@uw.edu>
2022-12-21 14:38:42 -0800

    Access camera, coordinator class and camera view support the new post view

---
+++ Coordinator.swift
@@ -0,0 +1,15 @@
+import UIKit
+class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    var picker: Camera
+    
+    init(picker: Camera) {
+        self.picker = picker
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let selectedImage = info[.originalImage] as? UIImage else { return }
+        self.picker.selectedImage = selectedImage
+        self.picker.isPresented.wrappedValue.dismiss()
+    }
+    
+}
---
+++ NewPostView.swift
@@ -0,0 +1,50 @@
+import SwiftUI
+
+struct NewPostView: View {
+    
+    @State private var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    @State private var selectedImage: UIImage?
+    @State private var isImagePickerDisplay = false
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                
+                if selectedImage != nil {
+                    Image(uiImage: selectedImage!)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(Circle())
+                        .frame(width: 300, height: 300)
+                } else {
+                    Image("fedW")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(Circle())
+                        .frame(width: 300, height: 300)
+                }
+                
+                Button("Choose a Photo") {
+                    self.sourceType = .camera
+                    self.isImagePickerDisplay.toggle()
+                }.padding()
+                
+                Button("Take a Photo") {
+                    self.sourceType = .photoLibrary
+                    self.isImagePickerDisplay.toggle()
+                }.padding()
+            }
+            .navigationBarTitle("Post to your GoalTree!")
+            .sheet(isPresented: self.$isImagePickerDisplay) {
+                Camera(selectedImage: self.$selectedImage, sourceType: self.sourceType)
+            }
+            
+        }
+    }
+}
+
+struct NewPostView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewPostView()
+    }
+}
