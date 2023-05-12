75256faf456a3dab53c73f1c51e8880307e261db
jkim236 <jkim236@uw.edu>
2023-05-12 09:31:42 -0700

    Added implementation mentioned in previous commit,  ready to merge into main

--- Goalshare tree2/TreeTabView.swift
+++ Goalshare tree2/TreeTabView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct TreeTabView: View {
     @EnvironmentObject var account: Account
     @Environment(\.presentationMode) var presentationMode
-    @State var currentSlide = 0
+    @State var currentSlide: Int
     @State var animationDone = false
     @State var addingMilestone = false
     var body: some View {
@@ -68,7 +68,7 @@ struct TreeTabView_Previews: PreviewProvider {
         let account2 = Account(username: "", password: "")
         account2.goals.append(contentsOf: [Goal(name: "goal1", date: Date(), color: .red), Goal(name: "goal2", date: Date(), color: .blue)])
         return VStack {
-            TreeTabView()
+            TreeTabView(currentSlide: 0)
                 .environmentObject(account2)
         }
     }
