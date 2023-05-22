ba8dca898d73b21e48f03e09764275124eb5b9ca
Joshua Kim <118137134+jkim236@users.noreply.github.com>
2023-05-21 21:07:28 -0700

    Revert "Goal circle UI implementation"

--- Goalshare tree2/Main.swift
+++ Goalshare tree2/Main.swift
@@ -7,31 +7,28 @@
 
 import Foundation
 import SwiftUI
-import Firebase
+
+
+
 
 @main
 struct Goalshare_tree2App: App {
-    init () {
-        FirebaseApp.configure()
-    }
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     @State var introducing = true
     var body: some Scene {
         WindowGroup {
-//            LoginView()
-            ContentView()
-//            Profile()
-//                .environmentObject(Account(username: "placeholder", password: "placeholder"))
-//                .onAppear {
-//                    UIDevice.current.setValue(UIInterfaceOrientation.portrait.rawValue, forKey: "orientation") // Forcing the rotation to portrait
-//                    AppDelegate.orientationLock = .portrait // And making sure it stays that way
-//                }.onDisappear {
-//                    AppDelegate.orientationLock = .all // Unlocking the rotation when leaving the view
-//                }
-//                .fullScreenCover(isPresented: $introducing) {
-//                    IntroductionVideo()
-//                        .environmentObject(MyState())
-//                }
+            Profile()
+                .environmentObject(Account(username: "placeholder", password: "placeholder"))
+                .onAppear {
+                    UIDevice.current.setValue(UIInterfaceOrientation.portrait.rawValue, forKey: "orientation") // Forcing the rotation to portrait
+                    AppDelegate.orientationLock = .portrait // And making sure it stays that way
+                }.onDisappear {
+                    AppDelegate.orientationLock = .all // Unlocking the rotation when leaving the view
+                }
+                .fullScreenCover(isPresented: $introducing) {
+                    IntroductionVideo()
+                        .environmentObject(MyState())
+                }
         }
     }
 }
--- Goalshare tree2/Profile.swift
+++ Goalshare tree2/Profile.swift
@@ -7,7 +7,6 @@
 
 import SwiftUI
 import UIKit
-
 struct Profile: View {
     @EnvironmentObject var account: Account
     @State private var isSlideUpViewPresented = false
