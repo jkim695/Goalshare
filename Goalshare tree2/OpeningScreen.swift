7a42383f1f7753710a82610cb7214505f4915fd0
Jkim695 <jkim695@jh.edu>
2023-12-20 19:04:20 -0500

    Opening screen added with login and register views being added into the file. Can now delete those separate views. Also would be nice to add a loading animation when log in or register button pressed

---
+++ Goalshare tree2/OpeningScreen.swift
@@ -0,0 +1,18 @@
+//
+//  OpeningScreen.swift
+//  Goalshare tree2
+//
+//  Created by Joshua Kim on 12/20/23.
+//
+
+import SwiftUI
+
+struct OpeningScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OpeningScreen()
+}
