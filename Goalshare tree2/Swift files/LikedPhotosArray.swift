5048f3175df657576ec44424c09ad5d90a9e334b
Jkim695 <jkim695@jh.edu>
2024-01-24 17:45:01 -0500

    Implementing like button, still have to store data in firebase

---
+++ Goalshare tree2/Swift files/LikedPhotosArray.swift
@@ -0,0 +1,8 @@
+//
+//  LikedPhotosArray.swift
+//  Goalshare tree2
+//
+//  Created by Joshua Kim on 1/24/24.
+//
+
+import Foundation
---
+++ Goalshare tree2/tempo.swift
@@ -0,0 +1,18 @@
+//
+//  tempo.swift
+//  Goalshare tree2
+//
+//  Created by Joshua Kim on 1/19/24.
+//
+
+import SwiftUI
+
+struct tempo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    tempo()
+}
